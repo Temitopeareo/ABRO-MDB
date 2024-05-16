28856dc036089c1186aff04bf9c16e9c53a60591
Temitopeareo <132304443+Temitopeareo@users.noreply.github.com>
2024-05-15 19:43:15 -0700

    ABRO-MD1

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 global.port =process.env.PORT
 global.appUrl=process.env.APP_URL || ""
-global.email ="astromedia0010@gmail.com"
-global.location="Astro,World"
+global.email ="abrahamtemitope247@gmail.com"
+global.location="Abro,World"
 
 
 global.mongodb= process.env.MONGODB_URI || ""
@@ -13,16 +13,16 @@ global.blockJids= process.env.BLOCK_JID || "null"
 global.DATABASE_URL = process.env.DATABASE_URL || ""
 
 global.timezone= process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
-global.github=process.env.GITHUB|| "https://github.com/Astropeda/Asta-Md";
-global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029VaPGt3QEwEjpBXT4Rv0z";
-global.website=process.env.GURL || "https://whatsapp.com/channel/0029VaPGt3QEwEjpBXT4Rv0z" ; 
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.imgur.com/JMsAFRD.jpeg" ;
-global.caption = process.env.CAPTION || global.caption || "αѕтα-м∂ 2024" 
+global.github=process.env.GITHUB|| "https://github.com/Temitopeareo/ABRO-MDB";
+global.gurl  =process.env.GURL  || "";
+global.website=process.env.GURL || "" ; 
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.imgur.com/7doiWDc.jpeg" ;
+global.caption = process.env.CAPTION || global.caption || "ABRO-MD 2024" 
 
 
-global.devs = "2348039607375"
-global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "2348039607375";
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "2348039607375";
+global.devs = "2348100151048"
+global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "23488100151048";
+global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "2348100151048";
 
 
 
@@ -45,13 +45,13 @@ global.waPresence= process.env.WAPRESENCE ||  "online" ;
 //========================= [ AUTO READ MSGS & CMDS ] =========================\\
 global.readcmds = process.env.READ_COMMAND || "false"
 global.readmessage = process.env.READ_MESSAGE || "false"
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || "2348039607375";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "2348100151048";
 
 
 global.read_status = process.env.AUTO_READ_STATUS || "false"
 global.save_status = process.env.AUTO_SAVE_STATUS || "false"
-global.save_status_from =  process.env.SAVE_STATUS_FROM  || "2348039607375";
-global.read_status_from =  process.env.READ_STATUS_FROM  ||  "2348039607375";
+global.save_status_from =  process.env.SAVE_STATUS_FROM  || "2348100151048";
+global.read_status_from =  process.env.READ_STATUS_FROM  ||  "2348100151048";
 
 global.api_smd = "https://api-smd.onrender.com"
 global.scan = "https://suhail-md-vtsf.onrender.com";
@@ -67,12 +67,12 @@ module.exports = {
   HANDLERS: process.env.PREFIX  || "/",
   BRANCH  : process.env.BRANCH  || "main",
   VERSION : process.env.VERSION || "3.0.0",
-  caption : global.caption || "αѕтα-м∂ 2024" , 
+  caption : global.caption || "ABRO-MD 2024" , 
  
-  author : process.env.PACK_AUTHER|| "αѕтяσ",
-  packname: process.env.PACK_NAME || "αѕтяσ",
-  botname : process.env.BOT_NAME  || "ᴀsᴛᴀ-ᴍᴅ",
-  ownername:process.env.OWNER_NAME|| "αѕтяσ",
+  author : process.env.PACK_AUTHER|| "ABRO",
+  packname: process.env.PACK_NAME || "ABRO",
+  botname : process.env.BOT_NAME  || "ABRO-MD",
+  ownername:process.env.OWNER_NAME|| "ABRO",
   errorChat : process.env.ERROR_CHAT || "",
   KOYEB_API : process.env.KOYEB_API  || "false",
   REMOVE_BG_KEY : process.env.REMOVE_BG_KEY  || "",
